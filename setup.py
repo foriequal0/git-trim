da63ee7c45f1973d8c80e5ad473899ed7225857e
SeongChan Lee <foriequal@gmail.com>
2020-02-06 11:15:24 +0900

    Remove all to rewrite in Rust

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="git cleanup",
-    version="0.1",
-    python_requires=">=3.6",
-    py_modules=["git_cleanup"],
-    entry_points={"console_scripts": ["git-cleanup = git_cleanup:main"]},
-)
