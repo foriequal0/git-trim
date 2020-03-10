c0acc88aca304274947b704c2325d28490d61ec7
SeongChan Lee <foriequal@gmail.com>
2020-03-10 10:35:16 +0900

    Do not push delete non `refs/heads/` remote branch

--- tests/hub_cli_checkout.rs
+++ tests/hub_cli_checkout.rs
@@ -45,11 +45,6 @@ fn fixture() -> Fixture {
             git push upstream feature:refs/pull/1/head
             git checkout master
         EOF
-        local <<EOF
-            git fetch ../origin feature:feature
-            git config branch.feature.remote "../origin"
-            git config branch.feature.merge "refs/heads/feature"
-        EOF
         "#,
     )
 }
@@ -68,6 +63,11 @@ fn test_accepted() -> Result<()> {
     let guard = fixture().prepare(
         "local",
         r#"
+        local <<EOF
+            git fetch ../origin feature:feature
+            git config branch.feature.remote "../origin"
+            git config branch.feature.merge "refs/heads/feature"
+        EOF
         upstream <<EOF
             git merge refs/pull/1/head
         EOF
@@ -95,6 +95,11 @@ fn test_accepted_but_forgot_to_delete() -> Result<()> {
     let guard = fixture().prepare(
         "local",
         r#"
+        local <<EOF
+            git fetch ../origin feature:feature
+            git config branch.feature.remote "../origin"
+            git config branch.feature.merge "refs/heads/feature"
+        EOF
         upstream <<EOF
             git merge refs/pull/1/head
         EOF
@@ -113,6 +118,34 @@ fn test_accepted_but_forgot_to_delete() -> Result<()> {
                     refname: "refs/heads/feature".to_string(),
                 },
             },
+            ..Default::default()
+        },
+    );
+    Ok(())
+}
+
+#[test]
+fn test_should_not_push_delete_non_heads() -> Result<()> {
+    let guard = fixture().prepare(
+        "local",
+        r#"
+        local <<EOF
+            git fetch ../upstream refs/pull/1/head:feature
+            git config branch.feature.remote "../origin"
+            git config branch.feature.merge "refs/pull/1/head"
+        EOF
+        upstream <<EOF
+            git merge refs/pull/1/head
+        EOF
+        "#,
+    )?;
+
+    let git = Git::try_from(Repository::open(guard.working_directory())?)?;
+    let branches = get_merged_or_gone(&git, &config())?;
+    assert_eq!(
+        branches.to_delete,
+        MergedOrGone {
+            merged_locals: set! {"feature"},
             ..Default::default()
         },
     );
