3e0cdccb6ab5da9092b6508b5ef9255af24ebed7
SeongChan Lee <foriequal@gmail.com>
2020-04-27 10:59:57 +0900

    Ensure docs are up-to-date

--- build-man.sh
+++ build-man.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-mkdir -p docs/
-cargo run --bin build-man --features build-man > docs/git-trim.1
-MANWIDTH=120 man docs/git-trim.1 > docs/git-trim.man
+MODE=$1
+case $MODE in
+build)
+    cargo build --bin build-man --features build-man
+    ;;
+run|"")
+    mkdir -p docs/
+    cargo run --bin build-man --features build-man > docs/git-trim.1
+    MANWIDTH=120 man docs/git-trim.1 > docs/git-trim.man
+    ;;
+*)
+    echo "Unknown mode: $MODE"
+    exit -1
+    ;;
+esac
